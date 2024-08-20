37b8be330906aac1b107a80aefa35642da3d59db
benjieben <148706993+appdev2023@users.noreply.github.com>
2024-08-20 12:46:33 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -171,36 +171,101 @@ document.addEventListener('DOMContentLoaded', function () {
         }); 
     }); 
 
-    const startScanningButton = document.getElementById('start-scanning');
-    const reader = document.getElementById('reader');
+    feather.replace();
 
-    startScanningButton.addEventListener('click', () => {
-        reader.style.display = 'block';
-        startScanningButton.style.display = 'none';
+    const controls = document.querySelector('.controls');
+    const cameraOptions = document.querySelector('.video-options>select');
+    const video = document.querySelector('video');
+    const canvas = document.querySelector('canvas');
+    const screenshotImage = document.querySelector('img');
+    const buttons = [...controls.querySelectorAll('button')];
+    let streamStarted = false;
+    
+    const [play, pause, screenshot] = buttons;
+    
+    const constraints = {
+      video: {
+        width: {
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440
+        },
+      }
+    };
+    
+    const getCameraSelection = async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
+      const options = videoDevices.map(videoDevice => {
+        return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+      });
+      cameraOptions.innerHTML = options.join('');
+    };
+    
+    play.onclick = () => {
+      if (streamStarted) {
+        video.play();
+        play.classList.add('d-none');
+        pause.classList.remove('d-none');
+        return;
+      }
+      if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+        const updatedConstraints = {
+          ...constraints,
+          deviceId: {
+            exact: cameraOptions.value
+          }
+        };
+        startStream(updatedConstraints);
+      }
+    };
+    
+    const startStream = async (constraints) => {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      handleStream(stream);
+    };
+    
+    const handleStream = (stream) => {
+      video.srcObject = stream;
+      play.classList.add('d-none');
+      pause.classList.remove('d-none');
+      screenshot.classList.remove('d-none');
+      streamStarted = true;
+    };
+    
+    getCameraSelection();
 
-        const html5QrCode = new html5QrCode("reader");
-        html5QrCode.start(
-            { facingMode: "environment" }, // Use rear camera
-            {
-                fps: 10,    // frames per second
-                qrbox: 250  // scanning box size
-            },
-            (decodedText, decodedResult) => {
-                // Handle the decoded QR code here
-                console.log(`QR Code detected: ${decodedText}`);
-                alert(`QR Code detected: ${decodedText}`);
-                html5QrCode.stop();
-                reader.style.display = 'none';
-                startScanningButton.style.display = 'block';
-            },
-            (errorMessage) => {
-                // Handle errors here
-                console.warn(`QR Code no match: ${errorMessage}`);
-            })
-            .catch((err) => {
-                // Handle exceptions here
-                console.error(`Unable to start scanning: ${err}`);
-            });
-    });
+    cameraOptions.onchange = () => {
+        const updatedConstraints = {
+          ...constraints,
+          deviceId: {
+            exact: cameraOptions.value
+          }
+        };
+        startStream(updatedConstraints);
+      };
+      
+      const pauseStream = () => {
+        video.pause();
+        play.classList.remove('d-none');
+        pause.classList.add('d-none');
+      };
+      
+      const doScreenshot = () => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d').drawImage(video, 0, 0);
+        screenshotImage.src = canvas.toDataURL('image/webp');
+        screenshotImage.classList.remove('d-none');
+      };
+      
+      pause.onclick = pauseStream;
+      screenshot.onclick = doScreenshot;
+    
     
 });
